38b848d482d57d7d432e6f75d503295d13a340e3
Светлана <sveta.romashko@inbox.ru>
2021-06-01 14:08:26 +0300

    Initial commit

---
+++ src/main/java/org/babareko/graduationBoot/GraduationBootApplication.java
@@ -0,0 +1,13 @@
+package org.babareko.graduationBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/babareko/graduationBoot/GraduationBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.babareko.graduationBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
