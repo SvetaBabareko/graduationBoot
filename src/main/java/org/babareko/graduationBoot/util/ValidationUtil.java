3f05356bd1feefe4a0937d2264ebff8e7d869fc8
Светлана <sveta.romashko@inbox.ru>
2021-06-03 14:17:04 +0300

    add new IllegalRequestDataException

---
+++ src/main/java/org/babareko/graduationBoot/util/ValidationUtil.java
@@ -0,0 +1,23 @@
+package org.babareko.graduationBoot.util;
+
+import lombok.experimental.UtilityClass;
+import org.babareko.graduationBoot.model.AbstractEntity;
+import org.babareko.graduationBoot.util.exception.IllegalRequestDataException;
+
+@UtilityClass
+public class ValidationUtil {
+    public static void checkNew(AbstractEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+    public static void assureIdConsistent(AbstractEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+}
--- src/main/java/org/babareko/graduationBoot/web/AccountController.java
+++ src/main/java/org/babareko/graduationBoot/web/AccountController.java
@@ -1,23 +1,76 @@
 package org.babareko.graduationBoot.web;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.babareko.graduationBoot.AuthUser;
+import org.babareko.graduationBoot.model.Role;
 import org.babareko.graduationBoot.model.User;
+import org.babareko.graduationBoot.repository.UserRepository;
+import org.babareko.graduationBoot.util.ValidationUtil;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.swing.text.html.parser.Entity;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.EnumSet;
 
 @RestController
 @RequestMapping(AccountController.URL)
+@AllArgsConstructor
+@Slf4j
 public class AccountController {
     static final String URL = "/api/account";
 
+    private final UserRepository userRepository;
+
     @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
     public User get(@AuthenticationPrincipal AuthUser authUser) {
-        // log.info("get {}", authUser);
+         log.info("get {}", authUser);
         // return ASSEMBLER.toModel(authUser.getUser());
         return authUser.getUser();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CacheEvict(value = "users", key = "#authUser.username")
+    public void delete(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("delete {}", authUser);
+        userRepository.deleteById(authUser.id());
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<User> register(@Valid @RequestBody User user) {
+        log.info("register {}", user);
+        ValidationUtil.checkNew(user);
+        user.setRoles(EnumSet.of(Role.USER));
+        user = userRepository.save(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/account")
+                .build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(user);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CachePut(value = "users", key = "#authUser.username")
+    public User update(@Valid @RequestBody User user, @AuthenticationPrincipal AuthUser authUser) {
+        log.info("update {} to {}", authUser, user);
+        User oldUser = authUser.getUser();
+        ValidationUtil.assureIdConsistent(user, oldUser.getId());
+        user.setRoles(oldUser.getRoles());
+        if (user.getPassword() == null) {
+            user.setPassword(oldUser.getPassword());
+        }
+        return userRepository.save(user);
     }
 }
