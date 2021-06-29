911e790ecc0e799344d638075a3e6b8e0d0fac8e
Светлана <sveta.romashko@inbox.ru>
2021-06-29 14:03:13 +0300

    add function create dish

--- src/main/java/org/babareko/graduationBoot/web/DishRestController.java
+++ src/main/java/org/babareko/graduationBoot/web/DishRestController.java
@@ -3,12 +3,17 @@ package org.babareko.graduationBoot.web;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.babareko.graduationBoot.model.Dish;
+import org.babareko.graduationBoot.model.Restaurant;
 import org.babareko.graduationBoot.repository.DishRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(DishRestController.URL)
@@ -21,7 +26,25 @@ public class DishRestController {
 
    @GetMapping
    public List<Dish> getAll() {
-        //log.info("get all dishes for restaurant {}", restaurantId);
+       log.info("get all dishes {}");
        return dishRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Dish> get(@PathVariable Integer id) {
+        log.info("get dish {}", id);
+        return dishRepository.findById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Dish> create(@Valid @RequestBody Dish dish) {
+        log.info("create dish {} for the restaurant {}", dish, dish.getRestaurant());
+        Dish created = dishRepository.save(dish);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+
 }
